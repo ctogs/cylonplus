dd2c49426c05a6e6b7b0b1f5b106da41ae6b45e7
Alex Yung <erg3kp@virginia.edu>
2024-03-29 17:22:51 -0400

    added import_csv to pandastests

---
+++ parallelpandas_testing/import_csv.py
@@ -0,0 +1,15 @@
+import pandas as pd
+
+REPEATS = 100000
+
+data_URL = "https://raw.githubusercontent.com/keitazoumana/Experimentation-Data/main/diabetes.csv"
+
+original_data = pd.read_csv(data_URL)
+
+# Duplicate each row 100000 times
+benchmarking_df = original_data.loc[original_data.index.repeat(REPEATS)]
+
+# Save the result as a CSV file for future use.
+file_name = f"benchmarking_data_{REPEATS*768}.csv"
+
+benchmarking_df.to_csv(file_name, index=False)
